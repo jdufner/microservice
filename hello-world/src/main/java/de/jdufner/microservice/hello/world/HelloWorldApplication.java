01c250c55aa831663dc5c616376c78515c8fa811
Juergen Dufner <juergen@jdufner.de>
2016-12-10 18:38:04 +0100

    CircuitBreaker Hystrix aktiviert und manuell getestet

--- hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldApplication.java
+++ hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldApplication.java
@@ -2,9 +2,11 @@ package de.jdufner.microservice.hello.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class HelloWorldApplication {
 
--- hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldController.java
+++ hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldController.java
@@ -1,5 +1,6 @@
 package de.jdufner.microservice.hello.world;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.ServiceInstance;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,10 +40,9 @@ public class HelloWorldController {
   }
 
   @RequestMapping(path = "/")
+  @HystrixCommand(fallbackMethod = "getPrimesUpTo100")
   public String helloWorld() {
-    String text = "Hello World!";
-    text = getPrimesSmallerThan(1000);
-    return text;
+    return getPrimesSmallerThan(1000);
   }
 
   private String getPrimesSmallerThan(int limit) {
@@ -59,4 +60,8 @@ public class HelloWorldController {
     return loadBalancerClient.choose(PRIMES).getUri();
   }
 
+  private String getPrimesUpTo100() {
+    List primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
+    return primes.toString();
+  }
 }
