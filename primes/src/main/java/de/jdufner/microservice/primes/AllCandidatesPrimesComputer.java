05199ca096e31d3326ac95f67edb0825b379e9b7
Juergen Dufner <juergen@jdufner.de>
2017-01-01 21:34:21 +0100

    Update Spring and refactor PrimesComputer to Strategy-Pattern
    * Update Spring 1.4.2 to 1.4.3
    * Refactor PrimesComputer to AbstractPrimesComputer with two subclasses
    * Configuration of strategy in application.properties
    * Add some Spring configuration

---
+++ primes/src/main/java/de/jdufner/microservice/primes/AllCandidatesPrimesComputer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016, Jürgen Dufner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.jdufner.microservice.primes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jürgen Dufner
+ * @since 0.0.1
+ */
+@Component
+@Scope("prototype")
+public class AllCandidatesPrimesComputer extends AbstractPrimesComputer {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AllCandidatesPrimesComputer.class);
+
+  @Override
+  protected boolean checkPrimeNumberCandidate(final int divisor, final int sqrt) {
+    for (int dividend = 2; dividend <= sqrt; dividend++) {
+      divisionCounter++;
+      if (isDivider(divisor, dividend)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
