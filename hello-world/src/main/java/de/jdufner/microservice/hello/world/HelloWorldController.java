0609ff66ff3c97fd0b0d84f0ab3d786c68e0471c
Juergen Dufner <juergen@jdufner.de>
2017-01-11 22:57:37 +0100

    Integrated a git-server as docker image and configurated services als configuration clients.
    * Build git-ssh-server based on https://github.com/unixtastic/git-ssh-server
    * Configured SSH-Keys in git-server and configuration server to connect to git
    * Configured business services als configuration clients
    * Delayed start of business services until configuration server fully started
    * Used https://github.com/vishnubob/wait-for-it for delay, but author currently
      doesn't maintain his project, therefore created fork https://github.com/jdufner/wait-for-it

--- hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldController.java
+++ hello-world/src/main/java/de/jdufner/microservice/hello/world/HelloWorldController.java
@@ -58,6 +58,9 @@ public class HelloWorldController {
   @Value("${arbitrary.configuration.value}")
   private String configurationValue = "Hardcoded Value";
 
+  @Value("${x.y.z}")
+  private String yast;
+
   @RequestMapping(path = "/")
   @HystrixCommand(fallbackMethod = "helloWorldFallback")
   public Greeting helloWorld(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
@@ -75,7 +78,7 @@ public class HelloWorldController {
     greeting.setMessage("Hello " + name + "!");
     greeting.setPrimes(primes);
     greeting.setHostname(getHostname());
-    greeting.setConfigurationValue(configurationValue);
+    greeting.setConfigurationValue(configurationValue + " " + yast);
     return greeting;
   }
 
